64fbb49d87c8d9694d0ef7b9bc7143fff30fe6ec
Collin Schwantes <schwantes@ecohealthalliance.org>
2022-03-04 16:19:42 -0500

    updated email utility script

--- R/utils-send-email.R
+++ R/utils-send-email.R
@@ -2,10 +2,13 @@
 #'
 #' @param to A vector of email addresses serving as primary recipients for the
 #'   message.
-#' @param from The email address of the sender. Default is
-#'   `rsconnectatecohealthalliance.org`.
+#' @param from The email address of the sender.
+#' @param attach Logical. Should reports be attached to email? Default is
+#'   FALSE. If TRUE, all HTML reports found in *"outputs"* folder will be
+#'   attached to email.
 #' @param test Logical. Is this an email alert for testing automation reports?
 #'   Default is FALSE.
+#' @param project_name String. Name of the project to use in email subject and body text.
 #'
 #' @return Invisible. Update email sent to list of recipients in `to`
 #'
@@ -13,7 +16,10 @@
 
 send_email_update <- function(to,
                               from = "rsconnect@ecohealthalliance.org",
+                              project_name,
+                              attach = FALSE,
                               test = FALSE) {
+  ## Set SMTP server credentials
   email_credentials <- blastula::creds_envvar(
     user = from,
     host = "smtp.gmail.com",
@@ -21,24 +27,29 @@ send_email_update <- function(to,
     use_ssl = TRUE
   )
 
+  ## Create human readable data and time
   readable_date_time <- blastula::add_readable_time()
 
+  ## List out reports found in "outputs" folder
   reports <- list.files("outputs", pattern = "\\.html")
 
+  ## Create links to HTML reports and create email
   if (test) {
-    report_links <- paste0(
-      Sys.getenv("URL_PREFIX"), "/", Sys.getenv("GITHUB_REF"),
-      "/", reports, "\n"
-    )
+    report_links <- sprintf("%s/%s/outputs/%s\n",
+                            Sys.getenv("URL_PREFIX"),
+                            Sys.getenv("GITHUB_REF"),
+                            reports)
+
 
     subject <- glue::glue(
-      "Testing Nipah Bangladesh Automated Reports for ", {readable_date_time}
+      "Testing {project_name} Automated Reports for ", {readable_date_time}
     )
 
     email <- blastula::compose_email(
       body = glue::glue(
         "The test automation reports can be viewed here: \n\n",
-        {report_links}, "\n\n"
+        {report_links}, "\n\n",
+        "A copy/copies of the {project_name} automated report/s is/are also attached. \n\n"
       ) |>
         blastula::md()
     )
@@ -48,18 +59,32 @@ send_email_update <- function(to,
     )
 
     subject <- glue::glue(
-      "Nipah Bangladesh Automated Reports for ", {readable_date_time}
+      "{project_name} Automated Reports for ", {readable_date_time}
     )
+
+    report_links_collapse <- glue::glue_collapse(report_links,sep = ", ",last = "and")
 
     email <- blastula::compose_email(
       body = glue::glue(
-        "The updated automation reports can be viewed here: \n\n",
-        {report_links}, "\n\n"
+        "The {project_name} automated reports can be viewed here: \n\n",
+        {report_links_collapse}, "\n\n",
+        "A copy/copies of the {project_name} automated report/s is/are also attached. \n\n"
       ) |>
         blastula::md()
     )
   }
 
+  ## Add attachements
+  if (attach) {
+    for (i in list.files("outputs", pattern = "\\.html$", full.names = TRUE)) {
+      email <- email |>
+        blastula::add_attachment(file = i)
+    }
+  } else {
+    email
+  }
+
+  ## Send email
   blastula::smtp_send(
     email = email,
     to = to,
